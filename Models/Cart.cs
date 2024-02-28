dc11d1d75fd61a0527626f5a84e9201bf32736bc
Attila Nagy <nagyattila86.hu@gmail.com>
2024-02-28 17:51:03 +0100

    Instantiate the products property of the cart type within the constructor
    Reason:
    Products were not able to be added the the list

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -3,5 +3,12 @@ namespace WebShop.Models
     public class Cart
     {
         public List<Product> Products { get; set; }
+
+        public User user { get; set; }
+
+        public Cart()
+        {
+            Products = new List<Product>();
+        }
     }
 }
